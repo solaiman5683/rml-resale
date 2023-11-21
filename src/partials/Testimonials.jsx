e3f1c13ea5b8fb8b90fbae3e39713be3873b7a83
joypaul1 <jyla5102555@gmail.com>
2023-11-21 16:51:21 +0600

    Testimonials

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OwlCarousel from "react-owl-carousel";
 
 function Testimonials() {
+  const [commentList, setCommentList] = useState([]);
+  useEffect(() => {
+    const fetchCommentData = async () => {
+      try {
+        const response = await fetch(
+          "http://202.40.181.98:9090/resale/web_api/version_1_0_1/client_comments.php",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              sis_id: "1",
+            },
+          }
+        );
+        const res = await response.json();
+        console.log(res.data);
+        if (res.status === "true") {
+          setCommentList(res.data);
+        } else {
+          console.error("API response status is not true:", res);
+        }
+      } catch (error) {
+        console.error("Error fetching COMMENT data:", error);
+      }
+    };
+
+    fetchCommentData();
+  }, []);
   //Owl Carousel Settings
   const options = {
     loop: true,
@@ -42,142 +70,36 @@ function Testimonials() {
           // loop
           // autoplay={true}
           className="testimonial-slider owl-carousel owl-theme"
-          {...options}>
-          <div className="testimonial-single">
-            <div className="testimonial-content">
-              <div className="testimonial-author-img">
-                <img src="../assets/img/testimonial/01.jpg" alt="log" />
+          {...options}
+        >
+          {commentList.map((commentItem, index) => {
+            return (
+              <div key={index} className="testimonial-single">
+                <div className="testimonial-content">
+                  <div className="testimonial-author-img">
+                    <img src={commentItem.PIC_URL} alt={commentItem.NAME} />
+                  </div>
+                  <div className="testimonial-author-info">
+                    <h4>{commentItem.NAME}</h4>
+                    <p>Customer</p>
+                  </div>
+                </div>
+                <div className="testimonial-quote">
+                  <span className="testimonial-quote-icon">
+                    <i className="flaticon-quote-hand-drawn-symbol"></i>
+                  </span>
+                  <p>{commentItem.COMMENTS}</p>
+                </div>
+                <div className="testimonial-rate">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
               </div>
-              <div className="testimonial-author-info">
-                <h4>Sylvia H Green</h4>
-                <p>Customer</p>
-              </div>
-            </div>
-            <div className="testimonial-quote">
-              <span className="testimonial-quote-icon">
-                <i className="flaticon-quote-hand-drawn-symbol"></i>
-              </span>
-              <p>
-                There are many variations of passages available but the majority
-                have suffered to the alteration in some injected.
-              </p>
-            </div>
-            <div className="testimonial-rate">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-            </div>
-          </div>
-          <div className="testimonial-single">
-            <div className="testimonial-content">
-              <div className="testimonial-author-img">
-                <img src="assets/img/testimonial/02.jpg" alt="img" />
-              </div>
-              <div className="testimonial-author-info">
-                <h4>Gordo Novak</h4>
-                <p>Customer</p>
-              </div>
-            </div>
-            <div className="testimonial-quote">
-              <span className="testimonial-quote-icon">
-                <i className="flaticon-quote-hand-drawn-symbol"></i>
-              </span>
-              <p>
-                There are many variations of passages available but the majority
-                have suffered to the alteration in some injected.
-              </p>
-            </div>
-            <div className="testimonial-rate">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-            </div>
-          </div>
-          <div className="testimonial-single">
-            <div className="testimonial-content">
-              <div className="testimonial-author-img">
-                <img src="assets/img/testimonial/03.jpg" alt="img" />
-              </div>
-              <div className="testimonial-author-info">
-                <h4>Reid E Butt</h4>
-                <p>Customer</p>
-              </div>
-            </div>
-            <div className="testimonial-quote">
-              <span className="testimonial-quote-icon">
-                <i className="flaticon-quote-hand-drawn-symbol"></i>
-              </span>
-              <p>
-                There are many variations of passages available but the majority
-                have suffered to the alteration in some injected.
-              </p>
-            </div>
-            <div className="testimonial-rate">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-            </div>
-          </div>
-          <div className="testimonial-single">
-            <div className="testimonial-content">
-              <div className="testimonial-author-img">
-                <img src="assets/img/testimonial/04.jpg" alt="img" />
-              </div>
-              <div className="testimonial-author-info">
-                <h4>Parker Jimenez</h4>
-                <p>Customer</p>
-              </div>
-            </div>
-            <div className="testimonial-quote">
-              <span className="testimonial-quote-icon">
-                <i className="flaticon-quote-hand-drawn-symbol"></i>
-              </span>
-              <p>
-                There are many variations of passages available but the majority
-                have suffered to the alteration in some injected.
-              </p>
-            </div>
-            <div className="testimonial-rate">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-            </div>
-          </div>
-          <div className="testimonial-single">
-            <div className="testimonial-content">
-              <div className="testimonial-author-img">
-                <img src="assets/img/testimonial/05.jpg" alt="img" />
-              </div>
-              <div className="testimonial-author-info">
-                <h4>Heruli Nez</h4>
-                <p>Customer</p>
-              </div>
-            </div>
-            <div className="testimonial-quote">
-              <span className="testimonial-quote-icon">
-                <i className="flaticon-quote-hand-drawn-symbol"></i>
-              </span>
-              <p>
-                There are many variations of passages available but the majority
-                have suffered to the alteration in some injected.
-              </p>
-            </div>
-            <div className="testimonial-rate">
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-              <i className="fas fa-star"></i>
-            </div>
-          </div>
+            );
+          })}
         </OwlCarousel>
       </div>
     </div>
